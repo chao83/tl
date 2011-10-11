bd80cffbd7edb393a98a5c27223b0f87388024a6
lichao <lichao@localhost>
2011-10-11 17:55:06 +0800

    ????????????16?16???

---
+++ src/source/RunDlg.h
@@ -0,0 +1,24 @@
+#ifndef TRAYLAUNCHER_RUNDLG_L
+#define TRAYLAUNCHER_RUNDLG_L
+
+icon_ptr & GRunIcon();
+HWND & GHdlgRun();
+
+const UINT UM_UPDATEHINT = WM_USER + 2;
+
+BOOL  CALLBACK RunDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
+using ns_file_str_ops::ShellSuccess;
+
+class CToolTip
+{
+	CToolTip(HWND hOwner, HWND hTool);
+	~CToolTip();
+
+	HWND m_hwndToolTip;
+};
+
+//! 设置历史记录
+void InitHistory(const std::vector<TSTRING>& vStrHis);
+
+#endif // TRAYLAUNCHER_RUNDLG_L
